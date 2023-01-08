bfb307bc62cebe53260eaa568204621cd41fcdbd
ArturLabenskyy <dodik.dod@hotmail.com>
2023-01-08 14:09:22 +0200

    2.2

---
+++ 2.2/2.2.js
@@ -0,0 +1,3 @@
+function tellFortune(jobTitle, location, partnerName, numberOfChildren){
+  console.log(`You will be a ${jobTitle} in ${location} married to ${partnerName} with ${numberOfChildren} children`);
+}
