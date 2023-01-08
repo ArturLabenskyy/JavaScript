42d683d16c66e2823baaef50f052d9ca50948749
ArturLabenskyy <dodik.dod@hotmail.com>
2023-01-08 12:23:58 +0200

    1.2

--- 1.2/1.2.js
+++ 1.2/1.2.js
@@ -26,3 +26,32 @@ a) number
 7. Does "100" + 30 produce a number or a string?
 a)number
 */
+
+//------Submit your answers under the  question
+//create three different ways to declare variables
+var firstWay = 1;
+let secondWay = 2;
+const thirdWay = 3;
+
+//declare a variable and reassign a value to it
+let num = 1;
+num = 2;
+
+//create a variable and after that assign a string with its value being: He's got it!
+
+
+
+//1. create a variable and assign a value on how much a restaurant bill is.
+let restaurantBill = 10;
+
+//2. create a variable and assign a value on how much tax they should pay.
+let restaurantTaxPercent = 17;
+
+//3. create a variable that will calculate the bill tax and its output would be: Your total bill is <total bill> $.
+let totalBill = restaurantBill + (restaurantBill / 100 * restaurantTaxPercent);
+
+// Round the number 50.6 to its nearest integer
+Math.round(50.6);
+
+//Create a variable that is undefined
+let undefinedVariable;
