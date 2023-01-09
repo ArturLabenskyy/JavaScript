8919c4a94b7c85124cfb98566679a59f6bd11164
ArturLabenskyy <dodik.dod@hotmail.com>
2023-01-09 11:01:55 +0200

    3.1

---
+++ 3.1/3.1.js
@@ -0,0 +1,77 @@
+/*
+JavaScript - Declaring Functions
+The following exercise contains the following
+subjects:
+* functions
+Instructions
+* Please reformat the following function
+declarations to function expression.
+* Please reformat the following function
+expressions to function declarations.
+Submit the file to Hive
+*/
+
+
+
+
+// From function declarations to function expression
+// -----------------------------------------------------------------------------
+function welcome() {
+let welcome = 'Welcome to Appleseeds Bootcamp!';
+return welcome;
+}
+
+const welcome = function() {
+    let welcome = 'Welcome to Appleseeds Bootscamp!';
+    return welcome;
+}
+// -----------------------------------------------------------------------------
+function power(a) {
+let myNumber = a;
+let result = Math.pow(myNumber, 2);
+return result;
+}
+
+const power = function(a) {
+    let myNumber = a;
+    let result = Math.pow(myNumber, 2);
+    return result;
+}
+// -----------------------------------------------------------------------------
+
+
+function add(a, b) {
+let myNumber = a;
+let sum = myNumber + b;
+return sum;
+}
+
+const add = function(a, b) {
+    let myNumber = a;
+    let sim = myNumber + b;
+    return sum;
+}
+// -----------------------------------------------------------------------------
+
+
+
+// From function expressions to function declarations
+// -----------------------------------------------------------------------------
+const hello = () => "Hello!";
+
+function hello() {
+    let hello = 'Hello!';
+    return hello;
+}
+// -----------------------------------------------------------------------------
+const squareRoot = a => Math.sqrt(a);
+
+function squareRoot(a) {
+    return Math.sqrt(a);
+}
+// -----------------------------------------------------------------------------
+const randomNumbers = (a, b) => Math.random() * (a - b) + b;
+
+function randomNumbers(a, b) {
+    return Math.random() * (a - b) + b;
+}
