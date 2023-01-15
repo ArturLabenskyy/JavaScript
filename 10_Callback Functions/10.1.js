bc5eeaf97189d9e247c4c0d6214964d163422ce6
ArturLabenskyy <dodik.dod@hotmail.com>
2023-01-15 14:44:05 +0200

    10.1

--- 10_Callback Functions/10.1.js
+++ 10_Callback Functions/10.1.js
@@ -1,11 +1,33 @@
-function printString(str) {
-    console.log(str);
+// --------------1-------------------
+// function printString(str) {
+//     console.log(str);
+// }
+
+// function isString(str, callback) {
+//     if (typeof str === `string`) {
+//         callback(str);
+//     }
+// }
+
+// isString(`it a string`, printString);
+// ---------------------------------------------------
+
+//----------------2 + 3--------------------------
+function dashes(str) {
+    let newStr = str.replaceAll(` `, `-`);
+    console.log(newStr);
+    return newStr;
 }
 
-function isString(str, callback) {
-    if (typeof str === `string`) {
-        callback(str);
-    }
+function firstWordToUpperCase(str, callback) {
+    // let arr = str.split(` `);
+    str = str.split(` `);
+    str[0] = str[0].toUpperCase();
+    str = str.toString();
+    str = str.replaceAll(`,`, ` `);
+    callback(str.replaceAll(`,`, ` `));
 }
 
-isString(`it a string`, printString);
+let string1 = `hello world ! ! !`;
+firstWordToUpperCase(string1, dashes);
+//----------------2-------------
